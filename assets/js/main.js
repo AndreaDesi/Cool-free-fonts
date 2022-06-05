cbca1b107960503c2d713c22bcd93de18236e4dd
AlessandroGallesi <alessandro.gallesi@gmail.com>
2022-06-05 12:55:08 +0200

    font page 1.3
    - Sistemati i pulsanti
    - Aggiunto pulsante download
    - Migliorata leggibilità js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,41 @@ $('#all-btn').click(function(){
   $('.sans-tag').show();
   $('.mono-tag').show();
 })
+
+
+//Font page
+
+//Random number function
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+//Sets --color3 to random number
+$(document).ready(function(){
+  let colore = randomInt(0, 255);
+  $('html').css('--color3', 'hsl('+colore+', 100%, 80%)');
+});
+
+
+//Swaps colors
+$('.swap-btn').click(function(){
+    $('.content').toggleClass('right').toggleClass('inv');
+    $('.swiper-button-prev').toggleClass('right').toggleClass('inv');
+    $('.swiper-button-next').toggleClass('right').toggleClass('inv');
+});
+
+//random color
+$('.rand-btn').click(function(){
+  let hue = randomInt(0, 255);
+  
+  $('html').css('--color2', 'hsl('+hue+', 100%, 80%)').css('--color3', 'var(--color2)');
+});
+
+//reset black and white
+$('.bw-btn').click(function(){
+  let hue = randomInt(0, 255);
+  if ($('.content').hasClass('inv')) {
+    $('.content').removeClass('inv').addClass('right');
+  }
+  $('html').css('--color2', 'white').css('--color3', 'hsl('+ hue +', 100%, 80%)');;
+});
